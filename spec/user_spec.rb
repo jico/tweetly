763415d9b79a2c41420a4efaacf8f60f013bbb68
Jico Baligod <jico@baligod.com>
2012-05-14 18:37:00 -0400

    fetching user timeline tests

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -10,4 +10,27 @@ describe Tweetly::User do
       @user.should be_an_instance_of Tweetly::User
     end
 	end
+
+	context "Fetching a user timeline" do
+		before do
+			@user = Tweetly::User.new('jicooo')
+		end
+
+		it "should fetch 1000 tweets by default" do
+			@user.fetch_timeline
+			@user.timeline.count.should == 1000
+		end
+
+		it "should be able to fetch less than 1000" do
+			@user.fetch_timeline(10)
+			@user.timeline.count.should == 10
+		end
+
+		it "should be able to fetch more than 1000" do
+			@user.fetch_timeline(1100)
+			@user.timeline.count.should == 1100
+		end
+	end
+
+
 end
