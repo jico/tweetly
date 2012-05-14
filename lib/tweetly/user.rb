071ffa25fdd0e5ae58131c39849a9ea5d046c413
Jico Baligod <jico@baligod.com>
2012-05-14 18:15:15 -0400

    word_freq and print_word_freq methods

--- lib/tweetly/user.rb
+++ lib/tweetly/user.rb
@@ -14,10 +14,50 @@ module Tweetly
 			end
 		end
 
+		# Build a word frequency array
+		#
+		def word_freq(options={})
+			# Default parameters
+			params = {
+				tweets: 1000
+			}
+			params.merge!(options)
+
+			# Fetch Tweets if necessary
+			fetch_timeline(params[:tweets]) if @timeline.count < params[:tweets]
+
+			freqDist = {}
+
+			@timeline.each do |tweet|
+				tweet.text.split.each do |word|
+					if freqDist.has_key? word
+						freqDist[word] += 1
+					else
+						freqDist[word] = 1
+					end
+				end
+			end
+			freqDist.sort_by { |k,v| -v }
+		end
+
+		# Print word frequency distribution
+		#
+		def print_word_freq(options={})
+			opts = {
+				tweets: 1000
+			}
+			opts.merge!(options)
+			dist = word_freq(opts)
+			opts[:count] ||= dist.count
+			dist[0,opts[:count]].each { |k,v| puts "#{k} (#{v})" }
+		end
+
 		# Fetches user timeline and caches it in the @timeline field.
 		#
+		# @todo Duplicate tweets due to max_id
+		#
 		# @param [Integer] count the number of Tweets to fetch
-		# @return [Boolean] Success or
+		# @return [Boolean] Success or failure (amount already cached)
 		def fetch_timeline(count=1000)
 			# Twitter API limits to 3200 tweets
 			count = [count, 3200].min
